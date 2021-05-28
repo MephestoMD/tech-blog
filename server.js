7b610d93ec977f7538093cb8f7ba2fe8cf7efccd
Maxson Green <maxson.green@gmail.com>
2021-05-28 13:49:55 -0400

    Created initial file structure

---
+++ server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const methodOverride = require('method-override');
+var compression = require('compression')
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+ const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {maxAge: 600000},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+app.use(methodOverride('_method'));
+app.use(compression());
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false })
+.then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+}).catch((err)=> {
+console.log(err.message);
+});
+  
